b3bee9ce46bc0c890cf6dfd047baac687208e972
itoktsnhc <itoktsnhc@hotmail.com>
2018-11-20 17:05:22 +0800

    upload

---
+++ Serilog.Sinks.AzureServiceBus/AzureServiceBus.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+
+namespace Serilog.Sinks.AzureServiceBus
+{
+    public class AzureServiceBus : ILogEventSink
+    {
+        private readonly IMessageSender _sender;
+        private readonly ITextFormatter _formatter;
+
+        public AzureServiceBus(IMessageSender sender, ITextFormatter formatter)
+        {
+            _sender = sender;
+            _formatter = formatter;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            byte[] body;
+            using (var render = new StringWriter())
+            {
+                _formatter.Format(logEvent, render);
+                body = Encoding.UTF8.GetBytes(render.ToString());
+            }
+
+            var message = new Message(body);
+            _sender.SendAsync(message).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+
+    public static class AzureServiceBusSinkExtensions
+    {
+        private const string DefaultOutputTemplate =
+            "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}";
+
+
+        public static LoggerConfiguration AzureServiceBus(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string connStr,
+            string entityPath,
+            string outputTemplate = DefaultOutputTemplate,
+            IFormatProvider formatProvider = null,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
+        {
+            if (loggerConfiguration == null) throw new ArgumentNullException(nameof(loggerConfiguration));
+            if (connStr == null) throw new ArgumentNullException(nameof(connStr));
+            if (entityPath == null) throw new ArgumentNullException(nameof(entityPath));
+            if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
+            if (formatProvider == null) throw new ArgumentNullException(nameof(formatProvider));
+
+            var sender = new MessageSender(connStr, entityPath);
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+            var sink = new AzureServiceBus(sender, formatter);
+            return loggerConfiguration.Sink(sink, restrictedToMinimumLevel);
+        }
+
+        public static LoggerConfiguration AzureServiceBus(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string connStr,
+            string entityPath,
+            ITextFormatter formatter,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
+        {
+            if (loggerConfiguration == null) throw new ArgumentNullException(nameof(loggerConfiguration));
+            if (formatter == null) throw new ArgumentNullException(nameof(formatter));
+            if (connStr == null) throw new ArgumentNullException(nameof(connStr));
+            if (entityPath == null) throw new ArgumentNullException(nameof(entityPath));
+
+            var sender = new MessageSender(connStr, entityPath);
+            var sink = new AzureServiceBus(sender, formatter);
+            return loggerConfiguration.Sink(sink, restrictedToMinimumLevel);
+
+        }
+    }
+}
---
+++ SinkTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Context;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.AzureServiceBus;
+
+namespace SinkTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connStr = "<FILL ME>";
+            var entityPath = "<FILL ME>";
+            var config = new LoggerConfiguration();
+            var logger = config.WriteTo.AzureServiceBus(connStr, entityPath, new JsonFormatter()).CreateLogger().ForContext<Program>();
+            logger.Error(new Exception(), "foobar");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
